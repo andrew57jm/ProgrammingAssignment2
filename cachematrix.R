ad5429c91fd65bcca50d7b7abfa702dbebd1ff82
Andrew J. Mendelsohn <ajm91@comcast.net>
2016-01-22 13:48:03 -0800

    Completed Assignment
    Implemented makeCacheMatrix and cacheInverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a 'special' matrix that caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cacheInv <- NULL
+    set <- function(y) {
+        # test that the input is a square matrix
+        if (!is.matrix(y)) stop("Input must be a matrix!")
+        # test that the input matrix is square
+        if (dim(y)[1]!=dim(y)[2]) stop("Input matrix must be square!")
+        
+        
+        # if the matrix is being set to a different matrix than what's
+        # cached, cache it and reset the cache Inverse
+        cacheMat <<- y
+        cacheInv <<- NULL
+    }
+    set(x)
+    get <- function() cacheMat
+    setinv <- function(inv) cacheInv <<- inv
+    getinv <- function() cacheInv
+    list (set = set, get=get, 
+          setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of a special matrix created with makeCacheMatrix
+## if the cached value exists, return it. otherwise calculate the inverse and
+## return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("returning cached inverse")
+        return(inv)
+    }
+    mat <- x$get()
+    inv = solve(mat,...)
+    x$setinv(inv)
+    inv
 }
